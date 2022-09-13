817164325a4110b225d818045cd878ef8d93845b
Zsolt Revay-Giran <zsolt.revay.g@gmail.com>
2022-09-14 01:04:21 +0200

    bump version number

--- packages/plugins/wps-blocks/wps-blocks.php
+++ packages/plugins/wps-blocks/wps-blocks.php
@@ -6,7 +6,7 @@
  * Author:          WPShapers
  * Author URI:      https://wpshapers.com
  * Text Domain:     wps-blocks
- * Version:         1.2.3
+ * Version:         1.3.0
  *
  * @package WpsBlocks
  */
